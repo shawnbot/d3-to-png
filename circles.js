54f214ca0f79a6de4f27268f1abe36794732bfde
Shawn Allen <shawn@stamen.com>
2013-10-15 18:18:13 -0700

    example script to generate SVG

---
+++ circles.js
@@ -0,0 +1,31 @@
+module.exports = function(window, options, callback) {
+  var d3 = window.d3,
+      width = options.width || 400,
+      height = options.width || 500;
+      count = options.count || 100,
+      svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+  var circles = d3.range(0, count)
+    .map(function(i) {
+      return {
+        x: Math.random() * width,
+        y: Math.random() * height,
+        r: 2 + Math.random() * 28
+      };
+    });
+
+  var color = d3.scale.category20();
+
+  svg.selectAll("circle")
+    .data(circles)
+    .enter()
+    .append("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", function(d) { return d.r; })
+      .attr("fill", function(d, i) { return color(i); });
+
+  callback(null, svg.node());
+};
