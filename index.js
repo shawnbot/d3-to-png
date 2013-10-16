816685810f18320636075daf420c5256a3f501fa
Shawn Allen <shawn@stamen.com>
2013-10-15 18:13:44 -0700

    this works now

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require("fs"),
+    sys = require("sys"),
+    jsdom = require("jsdom"),
+    argv = require("optimist").argv,
+    scripts = [
+        "http://d3js.org/d3.v3.min.js"
+    ],
+    render = require("./" + argv._[0]),
+    write = sys.puts;
+
+jsdom.env("<svg></svg>", scripts, function(errors, window) {
+    render(window, argv, function(error, node) {
+      node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+      write('<?xml version="1.0" standalone="yes"?>');
+      write(node.outerHTML);
+    });
+});
